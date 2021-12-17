5d376d47df840495d08b9bbd2be6342c5cae5504
Lian Studer <ln.studer@protonmail.ch>
2021-12-17 10:33:48 +0100

    remove posix type naming convention

--- main.c
+++ main.c
@@ -9,34 +9,18 @@ typedef struct {
     int ram;            // RAM in Gb
     int storage;        // Storage in Gb
     double cpu_clock;   // CPU clock in Ghz
-} thinkpad_t;
+} thinkpad;
 
-typedef struct card {
-    thinkpad_t *thinkpad;
-    struct card *next;
-    struct card *prev;
-} card_t;
-
-typedef struct {
-    card_t *top_card;
-
-} talon_t;
+typedef struct _card {
+    thinkpad *thinkpad;
+    struct _card *next;
+} card;
 
 /*
  * @author Lian Studer
  */
-card_t *new_card(thinkpad_t *model) {
-    card_t *new_card = (card_t *)malloc(sizeof(card_t));
-    new_card->thinkpad = model;
-    new_card->next = NULL; 
-    return new_card;
-}
-
-/*
- * @author Lian Studer
- */
-thinkpad_t *new_thinkpad(char *name, int ram, int storage, double cpu_clock) {
-    thinkpad_t *new_thinkpad = (thinkpad_t *)malloc(sizeof(thinkpad_t));
+thinkpad *new_thinkpad(char *name, int ram, int storage, double cpu_clock) {
+    thinkpad *new_thinkpad = (thinkpad *)malloc(sizeof(thinkpad));
     new_thinkpad->model_name = name;
     new_thinkpad->ram = ram;
     new_thinkpad->storage = storage;
@@ -47,20 +31,35 @@ thinkpad_t *new_thinkpad(char *name, int ram, int storage, double cpu_clock) {
 /*
  * @author Lian Studer
  */
-card_t *generate_cards() {
-    card_t *head = NULL;
+card *new_card(thinkpad *model) {
+    card *new_card = (card *)malloc(sizeof(card));
+    new_card->thinkpad = model;
+    new_card->next = NULL; 
+    return new_card;
+}
 
-    thinkpad_t *thinkpad1 = new_thinkpad("X220", 8, 256, 2.4);
-    card_t *card1 = new_card(thinkpad1);
+void append_card(card *top_card, card *new_card) {
+    new_card->next = top_card->next;
+    top_card->next = new_card;
+}
+
+/*
+ * @author Lian Studer
+ */
+card *generate_cards() {
+    card *head = NULL;
+
+    thinkpad *thinkpad1 = new_thinkpad("X220", 8, 256, 2.4);
+    card *card1 = new_card(thinkpad1);
     head = card1;
 
-    thinkpad_t *thinkpad2 = new_thinkpad("X420", 8, 512, 2.8);
-    card_t *card2 = new_card(thinkpad2);
+    thinkpad *thinkpad2 = new_thinkpad("X420", 8, 512, 2.8);
+    card *card2 = new_card(thinkpad2);
     head->next = card2;
     head = card2;
 
-    thinkpad_t *thinkpad3 = new_thinkpad("T60", 4, 64, 1.8);
-    card_t *card3 = new_card(thinkpad3);
+    thinkpad *thinkpad3 = new_thinkpad("T60", 4, 64, 1.8);
+    card *card3 = new_card(thinkpad3);
     head->next = card3;
     head = card3;
 
@@ -71,20 +70,14 @@ card_t *generate_cards() {
 /*
  * @author Lian Studer
  */
-bool run_game() {
-    card_t *full_stack = _generate_dummy_data();
+bool run_game() { 
+    card *top_card = generate_cards();
+    return true;
 }
 
 /*
  * @author Lian Studer
  */
 int main() {
-    card_t *head = _generate_dummy_data();
-
-    for (int i = 0; i < 10; i++) {
-        printf("ThinkPad Model: %s\n", head->thinkpad->model_name);
-        head = head->next;
-    }
-
-    return 0;
+    return run_game();
 }
