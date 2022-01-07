3ae08d977d642c3b4705497cdac1c42ed255e4ce
Lian Studer <ln.studer@protonmail.ch>
2022-01-07 10:58:18 +0100

    bogoooos

--- main.c
+++ main.c
@@ -1,20 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-
-/*
- * Function Prototypes
- */
-void eputs(char *);
-void nullcheck(void *, char *);
-thinkpad *create_thinkpad(char *, int, int, double);
-card *create_card(thinkpad *);
-cardstack *create_cardstack(thinkpad *);
-card *create_card(thinkpad *);
-void append_card(cardstack *, thinkpad *);
-cardstack *shuffle_cardstack();
-void generate_cards(void);
-bool run_game(void);
+#include <ctype.h>
 
 /*
  * Type Definitions
@@ -36,6 +23,22 @@ typedef struct {
     card *tail;
     size_t size;
 } cardstack;
+
+/*
+ * Function Prototypes
+ */
+void eputs(char *);
+void nullcheck(void *, char *);
+thinkpad *create_thinkpad(char *, int, int, double);
+card *create_card(thinkpad *);
+cardstack *create_cardstack(thinkpad *);
+card *create_card(thinkpad *);
+void append_card(cardstack *, thinkpad *);
+void shuffle_cardstack(cardstack *);
+void generate_cards(void);
+void run_game(void);
+void show_game_manual(void);
+char start_menu(void);
 
 /*
  * @author Kris Huber
@@ -71,13 +74,13 @@ thinkpad *create_thinkpad(char *name, int ram, int storage, double cpu_clock) {
  * @author Kris Huber
  * @description initialize a cardstack struct
  */
-cardstack *create_cardstack(thinkpad *tp) {
-    nullcheck(tp, "Thinpad Passed into LL Initializer");
-    cardstack *l = malloc(sizeof(cardstack));
-    l->tail = l->head = create_node(tp);
-    l->size = 1;
-    return l;
-}
+// cardstack *create_cardstack(thinkpad *tp) {
+//     nullcheck(tp, "Thinpad Passed into LL Initializer");
+//     cardstack *l = malloc(sizeof(cardstack));
+//     l->tail = l->head = create_node(tp);
+//     l->size = 1;
+//     return l;
+// }
 
 /*
  * @author Kris Huber
@@ -103,36 +106,107 @@ void append_card(cardstack *cs, thinkpad *tp) {
  * @author Kris Huber
  * @description shuffle all cards in cardstack
  */
-void shuffle_cardstack(cardstack *cs) {
-    card *current = cs-> head;
+// void shuffle_cardstack(cardstack *cs) {
+//     card *current = cs-> head;
 
-    // generate distinct set of random numbers in range of 1..cardstack->size
+//     // generate distinct set of random numbers in range of 1..cardstack->size
 
-    while(*current != NULL) {
+//     while(*current != NULL) {
         
-        current = current->next;
+//         current = current->next;
+//     }
+// }
+
+/*
+ * @author Lian Studer
+ */
+// void generate_cards() {
+//     cardstack *cs = create_cardstack(create_card(create_thinkpad("X220", 8, 256, 2.4)));
+//     append_card(cs, create_card(create_thinkpad("T420", 8, 512, 2.8)));
+//     append_card(cs, create_card(create_thinkpad("X60",  8, 64,  1.8)));
+// }
+
+/*
+ * @author Lian Studer
+ */
+void run_game() {
+    printf("\n\n--- Starting the game ---\n\n");
+
+    // cardstack *initial_cardstack = generate_cards();
+    // shuffle_cardstack(initial_cardstack);
+    // cardstack *second_cardstack = split_cardstack(initial_cardstack);
+
+    while (1) {
+        // Game and interaction logic        
     }
 }
 
 /*
  * @author Lian Studer
  */
-void generate_cards() {
-    cardstack *cs = create_cardstack(create_card(create_thinkpad("X220", 8, 256, 2.4)));
-    append_card(cs, create_card(create_thinkpad("T420", 8, 512, 2.8)));
-    append_card(cs, create_card(create_thinkpad("X60",  8, 64,  1.8)));
+void show_game_manual() {
+    printf("\n\n--- How to play ---\n\n");
 }
 
 /*
  * @author Lian Studer
  */
-bool run_game() { 
-    return true;
+char start_menu() {
+    char selected_option;
+    printf("Enter your selection: ");
+    scanf(" %c", &selected_option);
+     
+    if (isdigit(selected_option)) {
+        return selected_option;
+    }
+
+    printf("%c is not a valid menu option\n", selected_option);
+    return 0;
 }
 
 /*
  * @author Lian Studer
  */
 int main() {
+    // ASCII ThinkPad Logo
+    printf("\n \
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n \
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n \
+@@@..........@...@@@@@@@////@@@@@@@@@@@...@@@@@@@.......@@@@@@@@@@@@@@@@@...@@@\n \
+@@@@@@....@@@@...@@@@@@@@@@@@@@@@@@@@@@...@@@@@@@...@@...@@@@@@@@@@@@@@@@...@@@\n \
+@@@@@@....@@@@........@@...@@........@@...@@...@@...@@...@........@@........@@@\n \
+@@@@@@....@@@@...@@...@@...@@...@@...@@...@...@@@...@@...@...@@...@@...@@...@@@\n \
+@@@@@@....@@@@...@@...@@...@@...@@...@@...@...@@@.......@@...@....@@...@@...@@@\n \
+@@@@@@....@@@@...@@...@@...@@...@@...@@...,..@@@@...,*@@@@@.......@@...@@...@@@\n \
+@@@@@@....@@@@...@@...@@...@@...@@...@@...@...@@@...@@@@@@...@@...@@...@@...@@@\n \
+@@@@@@....@@@@...@@...@@...@@...@@...@@...@...@@@...@@@@@@...@@...@@...@@...@@@\n \
+@@@@@@....@@@@...@@...@@...@@...@@...@@...@@...@@...@@@@@@...@@...@@...@@...@@@\n \
+@@@@@@....@@@@...@@...@@...@@...@@...@@...@@...@@...@@@@@@....@...@@....@...@@@\n \
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n \
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n\n");
+    printf("ThinkPad® Quartett\nBy Kris Huber & Lian Studer\n\n");
+
+    printf("\n \
+Menu\n \
+1> Start game\n \
+2> How to play\n \
+3> Quit\n");
+
+    while (1) {
+        char menu_option = start_menu();
+        switch (menu_option) {
+            case '0':
+                continue;
+            case '1':
+                run_game();
+                break;
+            case '2':
+                show_game_manual();
+                break;
+            case '3':
+                break;
+        }
+    } 
+
     return 0;
 }
